f361931aa6ff70b40908a155b3f96a79ce77369d
Craig Meinschein <cmeinsch@gmail.com>
2015-02-23 16:24:05 -0800

    Added Get-UpdateHistory script.

---
+++ Get-UpdateHistory.ps1
@@ -0,0 +1,13 @@
+﻿$Session = New-Object -ComObject Microsoft.Update.Session
+$Searcher = $Session.CreateUpdateSearcher()
+$HistoryCount = $Searcher.GetTotalHistoryCount()
+$Updates = @()
+for ($i = 0; $i -lt $HistoryCount; $i++) {
+    $COMUpdate = $Searcher.QueryHistory($i,1)
+    $Update = New-Object -TypeName PSCustomObject
+    foreach($Property in ($COMUpdate | Get-Member -Type Property)) {
+        Add-Member -MemberType NoteProperty -Name $Property.Name -Value ($COMUpdate | Select-Object -ExpandProperty $Property.Name) -InputObject $Update
+	}
+    $Updates += $Update
+}
+$Updates
